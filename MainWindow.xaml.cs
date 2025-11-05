6db7859fc5a949de7a91a5c806ee2b8f2fae7b33
philippimboden06 <philippimboden06@gmail.com>
2025-11-05 14:59:18 +0100

    Design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,15 @@ namespace Fussballquiz
         {
             InitializeComponent();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
